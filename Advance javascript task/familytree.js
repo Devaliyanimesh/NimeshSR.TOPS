57d5fd4b4407e9c924714d9db81d4e3264f00b8d
Devaliyanimesh <131164253+Devaliyanimesh@users.noreply.github.com>
2023-09-06 12:48:28 +0530

    Add files via upload

---
+++ Advance javascript task/familytree.js
@@ -0,0 +1,80 @@
+let h = {
+  name: "nagjibhai",
+  age: 78,
+  son: [
+    {
+      name: "harsukhbhai",
+      age: 45,
+      ocuupation: "labour",
+      vehilcle: [
+        {
+          name: "splender",
+          type: "bike",
+        },
+        {
+          name: "thar",
+          type: "car",
+        },
+      ],
+      son: [
+        {
+          name: "nimesh",
+          age: 20,
+        },
+        {
+          name: "pritesh",
+          age: 22,
+        },
+      ],
+    },
+    {
+      name: "lalitbhai",
+      age: 48,
+      ocuupation: "labour",
+      vehilcle: [
+        {
+          name: "splender",
+          type: "bike",
+        },
+        {
+          name: "Range Rover",
+          type: "car",
+        },
+      ],
+      son: [
+        {
+          name: "hardik",
+          age: 22,
+        },
+        {
+          name: "sneha",
+          age: 24,
+        },
+      ],
+    },
+    {
+      name: "Rasikbhai",
+
+      age: 52,
+      ocuupation: "labour",
+      vehilcle: [
+        {
+          name: "shine",
+          type: "bike",
+        },
+        {
+          name: "BMW",
+          type: "car",
+        },
+      ],
+      son: {
+        name: "chiragh",
+        age: 25,
+      },
+    },
+  ],
+};
+
+console.log("familey tree:", h);
+
+
