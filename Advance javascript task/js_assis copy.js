e5a8085a927b880de600e7e553d29be89719aa6c
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-09-19 12:35:43 +0530

    done

---
+++ Advance javascript task/js_assis copy.js
@@ -0,0 +1,13 @@
+console.log(x);
+// console.log(y);
+// console.log(z);
+
+{
+  let x = 100;
+  //   var y = 100;
+  //   const z = 100;
+}
+
+console.log(x);
+// console.log(y);
+// console.log(z);
