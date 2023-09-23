7aeedca8258412b51f7e00db407572be5e8f1d32
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-09-23 14:49:03 +0530

    add

---
+++ Advance javascript task/js_assis/task-3.js
@@ -0,0 +1,43 @@
+// let currentDate = new Date();
+// let dat = currentDate.getDate();
+// let mon = currentDate.getMonth() + 1;
+// let year = currentDate.getFullYear();
+// let day = currentDate.getDay();
+// let mo = [
+//   "janu",
+//   "feb",
+//   "march",
+//   "april",
+//   "may",
+//   "jun",
+//   "july",
+//   "august",
+//   "sep",
+//   "octo",
+//   "nove",
+//   "dec",
+// ];
+// let week = [
+//   "Funday",
+//   "monday",
+//   "tuesday",
+//   "wenday",
+//   "thursday",
+//   "fridaday",
+//   "sturday",
+// ];
+
+// console.log(`today is ${year}/${mon}/${dat} ${week[day]}`);
+// console.log(`today is ${year}---${mo[mon]}---${dat} ${week[day]}`);
+
+// function fun() {
+//     for (let i = 1; i < 10; i--) {
+//       let a = Math.random() * 10;
+//       let b = Math.trunc(a);
+//       if (b <= 6) {
+//         return b;
+//       }
+//     }
+//   }
+//   let print = fun();
+//   console.log(" print:", print);
