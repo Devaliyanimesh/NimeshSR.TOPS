44309895a782cdf0d3cfa73e459294a779911b57
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-09-27 10:21:53 +0530

    add

--- Advance javascript task/practice/nullish_opretor.js
+++ Advance javascript task/practice/nullish_opretor.js
@@ -1,2 +1,13 @@
 // || return the first trutly value
-?? return the first defi
+// ?? return the first defi
+// ||
+// null ,undefined,0,false,this value are available in variable then second value show in output
+// let a = false;
+// let b = a || "test";
+// console.log("----> b:", b);
+
+//??
+//  null and undefined are availble in variable then second value show in output
+let a = 100;
+let b = a ?? "test";
+console.log("----> b:", b);
