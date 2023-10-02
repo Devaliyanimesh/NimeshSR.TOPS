38a8cff21e228aeb68bd4490d80c1bd8007e483c
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-02 10:54:38 +0530

    add

---
+++ Advance javascript task/js.asssi/03_array_object.js
@@ -0,0 +1,7 @@
+/*
+-> make data storage for your family and vehicle details of each family member
+-> all son of your dada name only
+-> all son of father/uncle's (note - me jname print thava joiye like uncle nu apu to uncle other wise dada nu apu to dad nu)
+-> badha family ni vehicale details like me j
+
+*/
---
+++ Advance javascript task/js.asssi/04_jsonTask/main.js
@@ -0,0 +1,43 @@
+let dataa = require("./data.json");
+let prod = require("./product.json");
+
+// 1. import data from json
+
+// Done
+
+// 2. give all city name of state which one give by user
+// function citybystate(statename) {
+//     let match = dataa.filter((e) => {
+//       return e.state_name === statename;
+//     });
+
+//     let data = match.map((e) => {
+//       return e.city;
+//     });
+//     return data;
+//   }
+
+//   let cityofkarnatak = citybystate("Delhi");
+//   console.log(" cityofkarnatak:", cityofkarnatak);
+
+// 3. give all city name , which have more character than user given input
+// 4. city list which has population more then user give
+
+// function fun(user, popu) {
+//   let state_namee = dataa.filter((e) => {
+//     return e.state_name === user && e.population > popu;
+//   });
+//   let cityy = state_namee.map((e) => {
+//     return e.city;
+//   });
+
+//   return cityy;
+// }
+
+// let final = fun("Delhi", "163716");
+// console.log(" final:", final);
+
+// 5. acending decending population wise
+// 6. top 5 city population wise [only name of city]
+// 7. give total populatin of state which give by user
+// 8. give top 5 state name population wise
