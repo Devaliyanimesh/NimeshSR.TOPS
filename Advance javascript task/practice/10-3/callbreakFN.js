799e10b7587d90383ac8b309747c45741cd7aa50
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-04 10:54:40 +0530

    add

--- Advance javascript task/practice/10-3/callbreakFN.js
+++ Advance javascript task/practice/10-3/callbreakFN.js
@@ -1,17 +1,36 @@
-function fun(funn) {
+// function fun1(cb) {
+//   setTimeout(() => {
+//     console.log("------>");
+//     cb();
+//   }, 5000);
+// }
+
+// function fun2(fun1) {
+//   console.log("------->second print");
+// }
+
+// function funn3() {
+//   console.log("------->third");
+// }
+
+// fun1(fun2);
+
+// callbreak hell ak ni adar ak hoy tene karvani rit
+
+function fun1(cb1, cb) {
   setTimeout(() => {
-    console.log("------>");
-    fun(fun1);
-    fun1(funn2);
-  }, 5000);
+    console.log("------>first");
+    cb(cb1);
+  }, 2000);
 }
 
-fun(funn2, fun1);
-
-function fun1(fun1) {
-  console.log("------->second print");
+function funn2(cb2) {
+  setTimeout(() => {
+    console.log("------>second");
+    cb2();
+  }, 2000);
 }
-
-function funn2() {
-  console.log("------->third");
+function funn3() {
+  console.log("---->third");
 }
+fun1(funn3, funn2);
