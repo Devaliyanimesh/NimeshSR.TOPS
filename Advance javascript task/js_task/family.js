75bdde1f07f1f522ce59f53a77d1b84817d8f6b8
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-06 11:22:29 +0530

    add

--- Advance javascript task/js_task/family.js
+++ Advance javascript task/js_task/family.js
@@ -2,8 +2,8 @@ let familytree = {
   name: "nagjibhai",
   age: 80,
   child: [
-    { 
-      id:1,  
+    {
+      id: 1,
       name: "harsukh",
       age: 38,
       ocuupation: "labour",
@@ -29,7 +29,7 @@ let familytree = {
       ],
     },
     {
-      id:2,
+      id: 2,
       name: "lalitbhai",
       age: 42,
       ocuupation: "labour",
@@ -47,10 +47,13 @@ let familytree = {
         {
           name: "hardik",
         },
+        {
+          name: "snehal",
+        },
       ],
     },
     {
-      id:3,
+      id: 3,
       name: "Rasikbhai",
       age: 50,
       ocuupation: "labour",
@@ -92,3 +95,19 @@ let familytree = {
 // console.log(familytree.child[1].son[1].age);
 
 //Third
+
+// find all child name by family id like -["jigar ", "kavya"]
+// function fun(user) {
+//   let fil = familytree.child.find((e) => {
+//     return e.id === user;
+//   });
+
+//   let fill = fil.son.map((e) => {
+//     return e["name"];
+//   });
+
+//   return fill;
+// }
+
+// let fill = fun(1);
+// console.log("fill", fill);
