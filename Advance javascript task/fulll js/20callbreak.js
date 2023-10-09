09865901f799ec5af0c8e6fdc63cbffe64e9cb58
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-09 13:08:28 +0530

    add

--- Advance javascript task/fulll js/20callbreak.js
+++ Advance javascript task/fulll js/20callbreak.js
@@ -1,28 +1,33 @@
-function fun1(arg, cb) {
+function fun1(arg3, arg2, arg, cb) {
   //   for (let i = 0; i < 1000000000; i++) {}
   setTimeout(() => {
     console.log("----->fun1");
-    cb(arg);
+    cb(arg3, arg2, arg);
   }, [2000]);
 }
 
-function fun2(cb2) {
+function fun2(arg3, arg2, cb1) {
   setTimeout(() => {
     console.log("----->fun2");
-    cb2();
+    cb1(arg3, arg2);
   }, 3000);
 }
 
-function fun3() {
+function fun3(arg3, cb2) {
   setTimeout(() => {
     console.log("----->fun3");
+    cb2(arg3);
   }, 2000);
 }
-//   function fun4() {
-//     console.log("----->fun4");
-//   }
+function fun4(cb3) {
+  console.log("----->fun4");
+  cb3();
+}
+function fun5() {
+  console.log("----->fun5");
+}
 
-fun1(fun3, fun2);
+fun1(fun5, fun4, fun3, fun2);
 
 /*
    models.Movie.create(input, (err, result) => {
