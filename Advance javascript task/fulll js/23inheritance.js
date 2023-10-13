1c90db20c12639dc53f38f3524213cf5c4bcd7c5
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-13 14:38:22 +0530

    add

---
+++ Advance javascript task/fulll js/23inheritance.js
@@ -0,0 +1,26 @@
+// class cm {
+//   login() {
+//     console.log("----->login");
+//   }
+//   salary() {
+//     console.log("---salary");
+//   }
+// }
+
+// class Teacher extends cm {}
+
+// class student extends cm {
+//   login() {
+//     super.login();
+//     console.log("----->stlogin");
+//   }
+//   fees() {
+//     console.log("-->stfees");
+//   }
+// }
+// const one = new Teacher();
+// // one.login();
+// // one.salary()
+// const two = new student();
+// two.login();
+// // two.salary()
