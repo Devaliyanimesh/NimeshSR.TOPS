d550624c346254f06bb5b55f75456c8807fd736b
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-16 12:50:49 +0530

    add

--- Advance javascript task/fulll js/Practic/02promice.js
+++ Advance javascript task/fulll js/Practic/02promice.js
@@ -1,10 +1,23 @@
 const pr = new Promise((resoved, reject) => {
   let one = [1, 2, 3, 4, 5, 6];
   let fin = one.includes(5);
-  if (fin === true) {
-    console.log("5 is availbale");
+  if (fin) {
+    resoved({ age: 20 });
   } else {
-    console.log("5 is not availble");
+    reject("5 is not availble");
   }
 });
-
+pr.then((str) => {
+  console.log("---->");
+  return { ...str, name: "nimesh" };
+})
+  .then((res1) => {
+    console.log("--->", res1);
+    return { ...res1, study: "it felid" };
+  })
+  .then((res2) => {
+    console.log("--->", res2);
+  })
+  .catch((error) => {
+    console.log("****---->", error.message);
+  });
