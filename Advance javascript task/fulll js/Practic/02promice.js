35b8f829d89d00125e1e10671263c74037c8805c
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-16 11:50:23 +0530

    add

---
+++ Advance javascript task/fulll js/Practic/02promice.js
@@ -0,0 +1,10 @@
+const pr = new Promise((resoved, reject) => {
+  let one = [1, 2, 3, 4, 5, 6];
+  let fin = one.includes(5);
+  if (fin === true) {
+    console.log("5 is availbale");
+  } else {
+    console.log("5 is not availble");
+  }
+});
+pr;
