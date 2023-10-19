a33e44b8542561450169b7fa67d15fadf37f736a
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-19 12:50:48 +0530

    add

---
+++ 5_javascript practic/pattern/01pate.js
@@ -0,0 +1,74 @@
+// 1.pattern
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// let one = 5;
+// let string = "";
+// for (let i = 1; i <= one; i++) {
+//   for (let h = 1; h <= i; h++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+// 2. pattern
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// let one = 5;
+// let string = "";
+// for (let i = 1; i <= one; i++) {
+//   for (let h = 1; h <= i; h++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+// 3 pattern
+// A
+// B B
+// C C C
+// D D D D
+// E E E E E
+
+// let n = 5;
+// let print = "";
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     print += String.fromCharCode(j + 64);
+//   }
+//   print += "\n";
+// }
+
+// console.log(print);
+
+// 4
+//         *
+//       * * *
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+
+// let n = 5;
+// let print = "";
+
+// for (let i = 1; i <= n; i++) {
+//   for (let spa = 1; spa <= n - i; spa++) {
+//     print += " ";
+//   }
+//   for (let j = 1; j <= 2 * i - 1; j++) {
+//     print += "*";
+//   }
+//   print += "\n";
+// }
+// console.log(print);
