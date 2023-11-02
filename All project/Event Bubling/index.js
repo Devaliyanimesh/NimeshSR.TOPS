6d9b6cf92ad621f69a6e0c059a748198fe103ba7
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-11-02 11:39:24 +0530

    add

--- All project/Event Bubling/index.js
+++ All project/Event Bubling/index.js
@@ -1,45 +1,53 @@
-let timer=3;
-let score=0;
-var hitrn=0;
+let timer = 10;
+let score = 0;
+var hitrn = 0;
 
-function incresescore(){
-    score +=10
-    document.querySelector("#scoreval").textContent=score
+function incresescore() {
+    score += 10
+    document.querySelector("#scoreval").textContent = score
+
+
 }
-function getnewhit(){
- hitrn=Math.floor(Math.random()*10)
-    document.querySelector("#hitval").textContent=hitrn
+function getnewhit() {
+    hitrn = Math.floor(Math.random() * 10)
+    document.querySelector("#hitval").textContent = hitrn
 }
-function makeBubble(){
-var clutter=""
-for(var i=1;i<=75;i++){
-    var rn=Math.floor(Math.random()*10)
-    clutter +=`<div class="bubble">${rn}</div>`
-}
-document.querySelector("#pbtm").innerHTML=clutter
-}
-function runTimer(){
-    var timerrint=setInterval(function(){
-        if(timer > 0){
-            timer--;
-            document.querySelector("#timervalue").textContent=timer;
-        }
-        else{
-    clearInterval(timerrint)
-    document.querySelector("#pbtm").innerHTML=`<h1>Game is over</h1>`
-        }
-    },1000)
+function makeBubble() {
+    var clutter = ""
+    for (var i = 1; i <= 75; i++) {
+        var rn = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble">${rn}</div>`
     }
+    document.querySelector("#pbtm").innerHTML = clutter
+}
+function runTimer() {
+    var timerrint = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timervalue").textContent = timer;
+        }
+        else {
+            clearInterval(timerrint)
+            if (score > 100) {
+                document.querySelector("#pbtm").innerHTML = `<h1> Yo are win scor are: ${score}  </h1>`
+            }
+            else {
+                document.querySelector("#pbtm").innerHTML = `<h1>Game is over</h1>`
 
-    document.querySelector("#pbtm")
-    .addEventListener("click",function(detailss){
-        var clicknum= Number(detailss.target.textContent)
-        if(clicknum=== hitrn){
+            }
+        }
+    }, 1000)
+}
+
+document.querySelector("#pbtm")
+    .addEventListener("click", function (detailss) {
+        var clicknum = Number(detailss.target.textContent)
+        if (clicknum === hitrn) {
             incresescore();
             makeBubble()
             getnewhit()
         }
-;
+        ;
     })
 
 runTimer()
