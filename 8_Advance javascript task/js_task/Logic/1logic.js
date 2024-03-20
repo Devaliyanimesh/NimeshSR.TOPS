b7fc5ca2567b87cc6aa2ead891d0a1a27074a1c3
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2024-03-20 19:30:21 +0530

    add

---
+++ 8_Advance javascript task/js_task/Logic/1logic.js
@@ -0,0 +1,12 @@
+
+function logic(amount){
+    // let note=[2000,500,200,50,20,10,5]
+    if (50 > amount) {
+        console.log("yes");
+    }
+    else {
+        console.log("no");
+    }
+
+}
+logic(2)
